68f3193dc22af7607fc586f46967e8a52dc4abfc
teejay-1999 <f2018-158@bnu.edu.pk>
2023-06-20 22:06:11 +0500

    removed unused functions

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -62,48 +62,4 @@ class CustomerController extends Controller
         $customer->save();
         return redirect()->route('customer.to_login');
     }
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Customer $customer)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Customer $customer)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Customer $customer)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Customer $customer)
-    {
-        //
-    }
 }
